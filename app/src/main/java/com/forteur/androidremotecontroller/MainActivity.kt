71c9fc2ee3b636b77688d942bfcae57f388c1ace
Daniele Della Cioppa <daniele.dellacioppa@gmail.com>
2024-05-08 15:07:33 +0100

    lazy Grid

--- app/src/main/java/com/forteur/androidremotecontroller/MainActivity.kt
+++ app/src/main/java/com/forteur/androidremotecontroller/MainActivity.kt
@@ -4,16 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
@@ -80,18 +90,47 @@ fun ButtonGroup(viewModel: TermuxViewModel, lifecycleOwner: LifecycleOwner) {
         Pair("Back", AdbCommands.BACK)
     )
 
-    commands.forEach { (label, details) ->
-        CommandButton(label, details.command, details.icon, viewModel, lifecycleOwner)
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(3), // Imposta il numero di colonne
+        contentPadding = PaddingValues(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(commands) { command ->
+            CommandCard(command.first, command.second.command, command.second.icon, viewModel, lifecycleOwner)
+        }
     }
 }
 
 @Composable
-fun CommandButton(label: String, command: Array<String>, icon: Int, viewModel: TermuxViewModel, lifecycleOwner: LifecycleOwner) {
-    Button(onClick = {
-        viewModel.sendCommand(lifecycleOwner, command[0], command.sliceArray(1 until command.size))
-    }) {
-        Image(painterResource(id = icon), contentDescription = null)
-        Text(label)
+fun CommandCard(label: String, command: Array<String>, icon: Int, viewModel: TermuxViewModel, lifecycleOwner: LifecycleOwner) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)  // Aggiunge spazio intorno alla card per una separazione visiva
+            .clickable {
+                viewModel.sendCommand(lifecycleOwner, command[0], command.sliceArray(1 until command.size))
+            },
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)  // Padding interno per spaziare uniformemente i contenuti
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,  // Centra il contenuto orizzontalmente
+            verticalArrangement = Arrangement.Center  // Centra il contenuto verticalmente
+        ) {
+            Image(
+                painter = painterResource(id = icon),
+                contentDescription = label,
+                modifier = Modifier
+                    .padding(bottom = 8.dp)  // Spazio tra l'icona e il testo
+            )
+            Text(
+                text = label,
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onSurface  // Assicura che il testo sia leggibile sullo sfondo della card
+            )
+        }
     }
 }
-
